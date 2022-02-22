59a2bcf8c17a948160207d06f7be17deffc4afc3
zhanglei <851106500@qq.com>
2022-02-22 14:04:23 +0800

    增加oss上传

---
+++ codingmore-admin/src/main/java/com/codingmore/config/OssClientConfig.java
@@ -0,0 +1,24 @@
+package com.codingmore.config;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class OssClientConfig {
+    // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+    @Value("${aliyun.oss.endpoint}")
+    String endpoint ;
+    // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+    @Value("${aliyun.oss.accessKeyId}")
+    String accessKeyId ;
+    @Value("${aliyun.oss.accessKeySecret}")
+    String accessKeySecret;
+
+    @Bean
+    public OSS createOssClient() {
+        return new  OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+    }
+}
---
+++ codingmore-admin/src/main/java/com/codingmore/service/impl/OssServiceImpl.java
@@ -0,0 +1,24 @@
+import com.aliyun.oss.OSS;
+import com.codingmore.service.IOssService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OssServiceImpl implements IOssService{
+   
+    @Value("${aliyun.oss.maxSize}")
+    private int maxSize;
+   
+    @Value("${aliyun.oss.bucketName}")
+    private String bucketName;
+  
+    @Value("${aliyun.oss.dir.prefix}")
+    private String dirPrefix;
+    
+    @Autowired
+    private OSS ossClient;
+
+
+}
