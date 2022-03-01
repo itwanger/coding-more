5a2ebbe48725263ff7d7f0efba4757060d76438f
zhanglei <851106500@qq.com>
2022-03-01 13:56:16 +0800

    并发上传

---
+++ codingmore-admin/src/main/java/com/codingmore/config/TreadPoolConfig.java
@@ -0,0 +1,43 @@
+package com.oschina.slaservice.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+@EnableAsync
+
+public class TreadPoolConfig implements AsyncConfigurer {
+    @Value("${async.executor.corePoolSize}")
+    private Integer corePoolSize;
+    @Value("${async.executor.maxPoolSize}")
+    private Integer maxPoolSize;
+    @Value("${async.executor.queueCapacity}")
+    private Integer queueCapacity;
+    @Value("${async.executor.keepAliveSeconds}")
+    private Integer keepAliveSeconds;
+    @Value("${async.executor.threadNamePrefix}")
+    private String threadNamePrefix;
+
+    @Bean(name = "ossUploadImageExecutor")
+    public ThreadPoolTaskExecutor getMetricsExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+
+        executor.setKeepAliveSeconds(keepAliveSeconds);
+        executor.setThreadNamePrefix(threadNamePrefix);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
+
+}
+  
--- codingmore-admin/src/test/java/com/codingmore/SampleTest.java
+++ codingmore-admin/src/test/java/com/codingmore/SampleTest.java
@@ -2,7 +2,10 @@ package com.codingmore;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Future;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import cn.hutool.core.io.file.FileReader;
@@ -22,9 +26,9 @@ import cn.hutool.core.io.file.FileReader;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SampleTest {
-//     http://cdn.tobebetterjavaer.com/
-// https://cdn.tobebetterjavaer.com/
-//https://cdn.tobebetterjavaer.com/codingmore/images/20220226/0fb602cc-13dd-4380-a08b-f80a6bf1ac37.jpg
+    // http://cdn.tobebetterjavaer.com/
+    // https://cdn.tobebetterjavaer.com/
+    // https://cdn.tobebetterjavaer.com/codingmore/images/20220226/0fb602cc-13dd-4380-a08b-f80a6bf1ac37.jpg
 
     @Autowired
     private UsersMapper userMapper;
@@ -33,36 +37,58 @@ public class SampleTest {
     private static Logger LOGGER = LoggerFactory.getLogger(SampleTest.class);
     @Autowired
     private IOssService iOssService;
-    
+
+    @Autowired
+    private ThreadPoolTaskExecutor ossUploadImageExecutor;
+
     @Test
     public void testSelect() {
-       String path =  iOssService.upload("https://pic1.zhimg.com/80/v2-5a6681d8b77b8f17a517f2858ea0bcd8_400x224.jpg?234324");
-       System.out.print(path);
+        String path = iOssService
+                .upload("https://pic1.zhimg.com/80/v2-5a6681d8b77b8f17a517f2858ea0bcd8_400x224.jpg?234324");
+        System.out.print(path);
     }
+
     @Test
     public void testInsert() {
         FileReader fileReader = FileReader.create(new File("D://能耗软件规划.md"),
-        Charset.forName("utf-8"));
+                Charset.forName("utf-8"));
         List<String> list = fileReader.readLines();
         StringBuilder stringBuilder = new StringBuilder();
-        list.forEach(item->{
+        list.forEach(item -> {
             stringBuilder.append(item);
         });
 
-
         String content = stringBuilder.toString();
         // System.out.println(content);
-        String pattern="!\\[[^\\]]+\\]\\([^)]+\\)";
+        String pattern = "!\\[[^\\]]+\\]\\([^)]+\\)";
 
         // StringBuffer operatorStr=new StringBuffer(content);
-        Pattern p = Pattern.compile(pattern,Pattern.CASE_INSENSITIVE);
+        Pattern p = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(content);
-        while(m.find()) {
-            //使用分组进行替换
+
+        Map<String, Future<String>> map = new HashMap<>();
+        // List<String> image
+        while (m.find()) {
+            // 使用分组进行替换
             LOGGER.info("{}", m.group());
-            // operatorStr.replace(m.start(2),m.end(2),"/");
-            // m = p.matcher(operatorStr);
+            String imageTag = m.group();
+            String imageUrl = imageTag.substring(imageTag.indexOf("(") + 1, imageTag.indexOf(")"));
+            Future<String> future = ossUploadImageExecutor.submit(() -> {
+                return iOssService.upload(imageUrl);
+            });
+            map.put(imageUrl, future);
         }
-        // return operatorStr.toString();
+        try {
+            for (String oldUrl : map.keySet()) {
+                Future<String> future = map.get(oldUrl);
+                String imageUrl = future.get(); // 获取返回结果 不阻塞
+                content = content.replace(oldUrl, imageUrl);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("{}", e);
+        }
+
+        System.out.println(content);
     }
 }
