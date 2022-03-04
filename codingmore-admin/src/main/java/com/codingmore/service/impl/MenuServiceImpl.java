9113d219d3946b04e41ed9054a3554a370cfef87
zhanglei <851106500@qq.com>
2022-03-04 09:21:26 +0800

    菜单

--- codingmore-admin/src/main/java/com/codingmore/service/impl/MenuServiceImpl.java
+++ codingmore-admin/src/main/java/com/codingmore/service/impl/MenuServiceImpl.java
@@ -1,10 +1,13 @@
 package com.codingmore.service.impl;
 
+import com.codingmore.dto.MenuNode;
 import com.codingmore.model.Menu;
 import com.codingmore.mapper.MenuMapper;
 import com.codingmore.service.IMenuService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +19,38 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IMenuService {
+    @Override
+    public int create(Menu menu) {
+        return 0;
+    }
 
+    @Override
+    public int update(Long id, Menu menu) {
+        return 0;
+    }
+
+    @Override
+    public Menu getItem(Long id) {
+        return null;
+    }
+
+    @Override
+    public int delete(Long id) {
+        return 0;
+    }
+
+    @Override
+    public List<Menu> list(Long parentId, Integer pageSize, Integer pageNum) {
+        return null;
+    }
+
+    @Override
+    public List<MenuNode> treeList() {
+        return null;
+    }
+
+    @Override
+    public int updateHidden(Long id, Integer hidden) {
+        return 0;
+    }
 }
---
+++ codingmore-mbg/src/main/java/com/codingmore/dto/MenuNode.java
@@ -0,0 +1,17 @@
+package com.codingmore.dto;
+
+import com.codingmore.model.Menu;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * 后台菜单节点封装
+ * Created by macro on 2020/2/4.
+ */
+@Data
+public class MenuNode extends Menu {
+    @ApiModelProperty(value = "子级菜单")
+    private List<MenuNode> children;
+}
