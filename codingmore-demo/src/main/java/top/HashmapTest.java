d55492ed8351f3da1f1a55530ec247cd08640f6b
itwanger <www.qing_gee@163.com>
2022-03-22 16:11:16 +0800

    配置文件不提交

---
+++ codingmore-demo/src/main/java/top/HashmapTest.java
@@ -0,0 +1,16 @@
+package top;
+
+import java.util.HashMap;
+
+/**
+ * 微信搜索「沉默王二」，回复 Java
+ *
+ * @author 沉默王二
+ * @date 3/12/22
+ */
+public class HashmapTest {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap<>();
+        hashMap.put("沉默王二", "阅读源码");
+    }
+}
