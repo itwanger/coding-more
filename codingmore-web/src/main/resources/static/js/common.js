f80bff75dc9de5a1fa676f56c940e51d337b0dc2
mforwow@126.com <mforwow@126.com>
2022-04-07 01:50:50 +0800

    添加搜索层、优化头部布局、头部加入菜单图标

--- codingmore-web/src/main/resources/static/js/common.js
+++ codingmore-web/src/main/resources/static/js/common.js
@@ -6,8 +6,39 @@ $(function() {
     //   NProgress.done()
     // })
   })
+  bindCommonEvents()
 })
 
-function OpenNewPage(url) {
+// 页面通用事件
+const bindCommonEvents = () => {
+  // 绑定搜索按钮点击事件
+  $('#searchBtn').on('click', () => {
+    setSearchLayerVisible(true)
+  })
+
+  // 绑定菜单点击事件
+  $('.menu-icon').one('click', () => { setSearchLayerVisible(true) })
+}
+
+const setSearchLayerVisible = val => {
+  console.log('setSearchLayerVisible=', val)
+  if (val) {
+    // 展开搜索层
+    let bottomStart = document.body.clientHeight - 50
+    $('.search-layer').css('bottom', bottomStart + 'px')
+    $('.search-layer').show().animate({ 'bottom': '0'}, 300)
+    $('.menu-icon').unbind().one('click', () => { setSearchLayerVisible(false) })
+  } else {
+    // 关闭搜索层
+    $('.search-layer').hide()
+    $('.menu-icon').unbind().one('click', () => { setSearchLayerVisible(true) })
+  }
+  // 展开、关闭通用代码
+  $('.menu-icon').toggleClass('opened')
+  $('.menu-icon > img, .main-area, #searchBtn').toggleClass('noshow') //.removeClass('noshow').siblings().addClass('none')
+}
+
+// 打开新页面方法
+function openNewPage(url) {
   window.open(url)
 }
