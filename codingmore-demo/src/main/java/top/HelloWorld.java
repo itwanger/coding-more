c70b8d869ca495c8a18b81ea89e3f9a37985e231
itwanger <www.qing_gee@163.com>
2022-05-23 07:30:20 +0800

    ceshi

--- codingmore-demo/src/main/java/top/HelloWorld.java
+++ codingmore-demo/src/main/java/top/HelloWorld.java
@@ -1,5 +1,8 @@
 package top;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * 微信搜索「沉默王二」，回复 Java
  *
@@ -10,5 +13,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.getProperties().list(System.out);
         System.out.println(System.getProperty("user.home"));
+
+        HashMap<String,String> map = new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("沉默王二");
+        list.forEach( e -> {
+            map.put(e, e.toLowerCase());
+        });
+        System.out.println(map);
     }
 }
